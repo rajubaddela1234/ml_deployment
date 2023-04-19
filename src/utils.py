7a022d7599e4505e4981d729f08b42158f334052
Raju <rajubaddela1234@gmail.com>
2023-04-19 19:20:42 +0530

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,7 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-
+    
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -34,7 +34,6 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
@@ -50,6 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
